1a497c57ce733fc99e596f7cf0e58ea44e5f3cd2
hardik-vasudev <hardikvasudev.universe@gmail.com>
2025-03-10 03:12:01 +0530

    Update it

--- patient-frontend/src/pages/WaitingScreen.jsx
+++ patient-frontend/src/pages/WaitingScreen.jsx
@@ -10,7 +10,8 @@ const WaitingScreen = () => {
   const [loadingJwt, setLoadingJwt] = useState(false);
   const navigate = useNavigate();
 
-  const BASE_URL = process.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+
 
   const handleFetchPatientData = async () => {
     try {
